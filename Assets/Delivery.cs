eafbe730ffc8a345cf7e215a591176eee9628bae
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-09-21 11:48:04 -0400

    Video 05: Bool

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
+    void Start()
+    {
+        Debug.Log(hasPackage);
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Package"))
         {
             Debug.Log("Picked up package");
+            hasPackage = true;
         }
 
-        if (collision.CompareTag("Customer"))
+        if (collision.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered to customer");
+            hasPackage = false;
         }
     }
 }
